c822154598c8d0f0569eb61870ffadc6f35121c5
ibrahim <ibrahimabiodun069@gmail.com>
2026-07-01 14:14:10 +0100

    'commit'

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ app.get("/", (req, res) => {
     res.send("Home works");
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log("Server running");
-});
+// app.listen(PORT, () => {
+//     console.log("Server running");
+// });
+
+module.exports = app;
