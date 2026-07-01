23441f43253dec427bd0fc4316f247e0a2bc7134
ibrahim <ibrahimabiodun069@gmail.com>
2026-07-01 13:19:49 +0100

    'commit'

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
-const env = require('dotenv').config({quiet: true});
-const mongoose = require('./connections/mongodb.connection');
-const PORT = process.env.PORT || 5000;
-const cors = require('cors');
-const userRoute = require('./routes/user.route');
 
-// Middleware
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use((req, res, next) => {
-//     console.log(req.method, req.url);
-//     next();
-// });
 
-app.use((req, res, next) => {
-    console.log("Incoming request:", req.method, req.url);
-    next();
+app.get("/", (req, res) => {
+    res.send("Home works");
 });
 
-app.use('/', userRoute);
+app.post("/signup", (req, res) => {
+    console.log("Reached signup");
+    console.log(req.body);
 
-app.get("/ping", (req, res) => {
-    console.log("Ping received");
-    res.send("pong");
+    res.json({
+        success: true,
+        body: req.body
+    });
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log("Server started");
 });
