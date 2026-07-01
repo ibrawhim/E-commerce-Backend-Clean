95485fe88f3f277ea022eb664b86ea8be922b856
ibrahim <ibrahimabiodun069@gmail.com>
2026-07-01 14:58:33 +0100

    'commit'

--- controllers/userInfos.controller.js
+++ controllers/userInfos.controller.js
@@ -2,7 +2,15 @@ const signupModel = require('../models/signup.model');
 const signinModel = require('../models/signin.model');
 
 const signupController = (req, res) => {
-    console.log(req.body);
+    let form = new signupModel(req.body);
+    form.save((err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(400).json({ message: 'Something went wrong' });
+        } else {
+            res.status(201).json({ message: 'Signup successful', data });
+        }
+    });
 };
 
 const signinController = (req, res) => {
