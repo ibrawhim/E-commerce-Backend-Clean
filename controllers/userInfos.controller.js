905700074dce4a8011a5321648c479fb023f1999
ibrahim <ibrahimabiodun069@gmail.com>
2026-07-09 22:19:10 +0100

    token'

--- controllers/userInfos.controller.js
+++ controllers/userInfos.controller.js
@@ -26,18 +26,18 @@ const { email, password } = req.body;
           message: "Email not found"
         });
       }
-      let secret = process.env.SECRET;
       user.validatePassword(password, (err, same) => {
         if (err) {
           return res.status(500).send(err);
         }
-
+        
         if (!same) {
           return res.send({
             status: false,
             message: "Invalid Password"
           });
         }    
+        let secret = process.env.SECRET;
         const userData = user.toObject(); 
         delete userData.password;         
         let token = jwt.sign({email},secret,{expiresIn: '24h'})
